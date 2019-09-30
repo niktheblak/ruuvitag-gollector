2618ffa895304e6a0330d93285a295faaeb05b00
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2019-09-30 09:56:28 +0300

    Added unit test for continuous scan

--- pkg/scanner/scanner_test.go
+++ pkg/scanner/scanner_test.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/go-ble/ble"
-	"github.com/niktheblak/ruuvitag-gollector/pkg/config"
 	"github.com/niktheblak/ruuvitag-gollector/pkg/exporter"
 	"github.com/niktheblak/ruuvitag-gollector/pkg/sensor"
 	"github.com/stretchr/testify/assert"
@@ -168,11 +167,10 @@ var testData = sensor.DataFormat3{
 }
 
 func TestScanOnce(t *testing.T) {
-	cfg := config.Config{}
 	peripherals := map[string]string{
 		"cc:ca:7e:52:cc:34": "Test",
 	}
-	scn, err := New(cfg.ReportingInterval.Duration, cfg.Device, peripherals)
+	scn, err := New(1*time.Second, "default", peripherals)
 	require.NoError(t, err)
 	exp := new(mockExporter)
 	scn.Exporters = []exporter.Exporter{exp}
@@ -188,8 +186,39 @@ func TestScanOnce(t *testing.T) {
 	defer cancel()
 	err = scn.ScanOnce(ctx)
 	require.NoError(t, err)
+	assert.NotEmpty(t, exp.events)
+	e := exp.events[0]
+	assert.Equal(t, "Test", e.Name)
+	assert.Equal(t, "cc:ca:7e:52:cc:34", e.Addr)
+	assert.Equal(t, 55.0, e.Temperature)
+	assert.Equal(t, 60.0, e.Humidity)
+	assert.Equal(t, 51000, e.Pressure)
+	assert.Equal(t, 500, e.Battery)
+}
+
+func TestScan(t *testing.T) {
+	peripherals := map[string]string{
+		"cc:ca:7e:52:cc:34": "Test",
+	}
+	scn, err := New(1*time.Second, "default", peripherals)
+	require.NoError(t, err)
+	exp := new(mockExporter)
+	scn.Exporters = []exporter.Exporter{exp}
+	buf := new(bytes.Buffer)
+	err = binary.Write(buf, binary.BigEndian, testData)
+	require.NoError(t, err)
+	device := mockDevice{}
+	scn.ble = mockBLEScanner{
+		manufacturerData: buf.Bytes(),
+	}
+	scn.dev = mockDeviceCreator{device: device}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = scn.Start(ctx)
+	require.NoError(t, err)
 	// Wait a bit for messages to appear in the measurements channel
-	//time.Sleep(100 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
+	scn.Stop()
 	assert.NotEmpty(t, exp.events)
 	e := exp.events[0]
 	assert.Equal(t, "Test", e.Name)
