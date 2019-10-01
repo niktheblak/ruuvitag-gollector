89924e854d62c3c430e621e4a4fb2ec90413e505
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2019-10-01 10:21:32 +0300

    Unit test for InfluxDB exporter

---
+++ pkg/exporter/influxdb/influxdb_test.go
@@ -0,0 +1,44 @@
+package influxdb
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/niktheblak/ruuvitag-gollector/pkg/sensor"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReport(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msg, err := ioutil.ReadAll(r.Body)
+		require.NoError(t, err)
+		assert.Equal(t, ",mac=CC:CA:7E:52:CC:34,name=Backyard humidity=45,pressure=1002,temperature=22.1 1569924000000000000\n", string(msg))
+		w.WriteHeader(http.StatusNoContent)
+		_, err = w.Write([]byte(""))
+		require.NoError(t, err)
+	}))
+	defer srv.Close()
+	exporter, err := New(Config{
+		Addr: srv.URL,
+	})
+	require.NoError(t, err)
+	ctx := context.Background()
+	err = exporter.Export(ctx, sensor.Data{
+		Addr:          "CC:CA:7E:52:CC:34",
+		Name:          "Backyard",
+		Temperature:   22.1,
+		Humidity:      45.0,
+		Pressure:      1002.0,
+		Battery:       50,
+		AccelerationX: 0,
+		AccelerationY: 0,
+		AccelerationZ: 0,
+		Timestamp:     time.Date(2019, time.October, 1, 10, 0, 0, 0, time.UTC),
+	})
+	require.NoError(t, err)
+}
