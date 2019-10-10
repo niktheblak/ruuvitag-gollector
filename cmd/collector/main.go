dfcf228767723d3d601437eb574b255d875a5aef
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2019-10-10 22:21:16 +0300

    Fixed device config

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -39,7 +39,7 @@ func run(c *cli.Context) error {
 		defer client.Close()
 		logger = client.Logger("ruuvitag-gollector").StandardLogger(logging.Info)
 	}
-	scn, err := scanner.New(logger, c.String("device"), ruuviTags)
+	scn, err := scanner.New(logger, c.GlobalString("device"), ruuviTags)
 	if err != nil {
 		return fmt.Errorf("failed to create scanner: %w", err)
 	}
