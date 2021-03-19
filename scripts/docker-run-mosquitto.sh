cd295d6416055564611e3e7619c4b69227699923
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2021-03-19 12:29:32 +0200

    Fix shellcheck alerts

--- scripts/docker-run-mosquitto.sh
+++ scripts/docker-run-mosquitto.sh
@@ -2,7 +2,7 @@
 
 mosquitto_config=$(mktemp)
 echo "listener 1883
-allow_anonymous true" > $mosquitto_config
+allow_anonymous true" > "$mosquitto_config"
 
 docker run \
   -it \
@@ -10,7 +10,7 @@ docker run \
   --name mosquitto \
   --network ruuvitag \
   -p 1883:1883 \
-  -v $mosquitto_config:/mosquitto/config/mosquitto.conf\
+  -v "$mosquitto_config":/mosquitto/config/mosquitto.conf\
   eclipse-mosquitto:2.0.8
 
-rm $mosquitto_config
+rm "$mosquitto_config"
--- scripts/postgres-schema.sh
+++ scripts/postgres-schema.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ./docker-run.sh \
   postgres-schema \
   --postgres.conn "postgresql://ruuvitag:changeme@postgres:5432/ruuvitag?sslmode=disable" \
