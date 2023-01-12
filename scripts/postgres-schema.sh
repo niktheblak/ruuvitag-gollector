1ebab39049d2e923349bab32467b051d960d57ed
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2023-01-12 23:30:35 +0200

    Remove obsolete script

--- scripts/postgres-schema.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./docker-run.sh \
-  postgres-schema \
-  --postgres.conn "postgresql://ruuvitag:changeme@postgres:5432/ruuvitag?sslmode=disable" \
-  --postgres.table measurements
