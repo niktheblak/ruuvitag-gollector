d317033a3420bf48c55eab9565f2c8a46597ead0
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2023-01-12 23:28:33 +0200

    Update systemd configs

--- init/run-ruuvitag-gollector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo /home/pi/bin/ruuvitag-gollector \
-  --config /home/pi/ruuvitag-gollector/configs/pi.toml \
-  -d \
-  --scan_interval 5m \
-  --influxdb \
-  --pubsub \
-  --stackdriver
