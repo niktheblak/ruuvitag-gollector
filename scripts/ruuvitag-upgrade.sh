6f8fbb35612391ed34baadd0ca573d47bed80a4e
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-07-01 16:58:52 +0300

    chore: assume scripts are run from root dir

--- scripts/ruuvitag-upgrade.sh
+++ scripts/ruuvitag-upgrade.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-pushd .
-cd ..
 sudo systemctl stop ruuvitag-gollector.service
 sudo cp ruuvitag-gollector /usr/local/bin/
 sudo systemctl start ruuvitag-gollector.service
-popd
