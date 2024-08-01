597c1d696ad8e2cf188fce2f728c09062f9639eb
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-08-01 12:30:38 +0300

    fix: dummy postgres exporter constructor signature

--- pkg/exporter/postgres/postgres_dummy.go
+++ pkg/exporter/postgres/postgres_dummy.go
@@ -2,6 +2,12 @@
 
 package postgres
 
+import (
+	"context"
+
+	"github.com/niktheblak/ruuvitag-gollector/pkg/exporter"
+)
+
 func New(ctx context.Context, cfg Config) (exporter.Exporter, error) {
 	return exporter.NoOp{ReportedName: "Postgres"}, nil
 }
