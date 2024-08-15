e419c51768f9dd639d071e14336edec0c5c963bf
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-08-15 16:54:16 +0300

    refactor: use pgx-reconnect for reconnection

--- go.mod
+++ go.mod
@@ -33,7 +33,8 @@ require (
 
 require (
 	github.com/carlmjohnson/versioninfo v0.22.5
-	github.com/jackc/pgx/v5 v5.6.0
+	github.com/cenkalti/backoff/v4 v4.3.0
+	github.com/niktheblak/pgx-reconnect v1.0.0
 	github.com/niktheblak/ruuvitag-common v1.3.2
 )
 
@@ -57,6 +58,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
--- pkg/exporter/postgres/postgres.go
+++ pkg/exporter/postgres/postgres.go
@@ -9,7 +9,8 @@ import (
 	"log/slog"
 	"time"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/niktheblak/pgx-reconnect"
 	"github.com/niktheblak/ruuvitag-common/pkg/psql"
 	"github.com/niktheblak/ruuvitag-common/pkg/sensor"
 
@@ -17,7 +18,7 @@ import (
 )
 
 type postgresExporter struct {
-	conn       *pgx.Conn
+	conn       *pgxreconnect.ReConn
 	connString string
 	query      string
 	columns    map[string]string
@@ -35,18 +36,23 @@ func New(ctx context.Context, cfg Config) (exporter.Exporter, error) {
 		cfg.Logger = slog.New(slog.NewTextHandler(io.Discard, nil))
 	}
 	cfg.Logger = cfg.Logger.With("exporter", "PostgreSQL")
+	conn, err := pgxreconnect.Connect(ctx, cfg.ConnString, backoff.NewExponentialBackOff())
+	if err != nil {
+		return nil, err
+	}
 	q, err := psql.BuildInsertQuery(cfg.Table, cfg.Columns)
 	if err != nil {
 		return nil, err
 	}
 	cfg.Logger.LogAttrs(ctx, slog.LevelDebug, "Using insert query", slog.String("query", q))
 	e := &postgresExporter{
+		conn:       conn,
 		connString: cfg.ConnString,
 		query:      q,
 		columns:    cfg.Columns,
 		logger:     cfg.Logger,
 	}
-	return e, e.reconnect(ctx)
+	return e, nil
 }
 
 func (t *postgresExporter) Name() string {
@@ -54,51 +60,17 @@ func (t *postgresExporter) Name() string {
 }
 
 func (t *postgresExporter) Export(ctx context.Context, data sensor.Data) error {
-	// previous reconnect failed; attempt again
-	if t.conn == nil {
-		if err := t.reconnect(ctx); err != nil {
-			return fmt.Errorf("failed to reconnect: %w", err)
-		}
-	}
 	args := psql.BuildQueryArguments(t.columns, data)
 	_, err := t.conn.Exec(ctx, t.query, args...)
 	if err != nil {
-		if err.Error() == "conn closed" {
-			// reconnect and retry
-			if reconnectErr := t.reconnect(ctx); reconnectErr != nil {
-				return fmt.Errorf("failed to reconnect: %w", reconnectErr)
-			}
-			_, err = t.conn.Exec(ctx, t.query, args...)
-			return err
-		} else {
-			return err
-		}
+		return err
 	}
 	return nil
 }
 
 func (t *postgresExporter) Close() error {
-	if t.conn == nil || t.conn.IsClosed() {
-		return nil
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	err := t.conn.Close(ctx)
-	t.conn = nil
 	return err
-}
-
-func (t *postgresExporter) reconnect(ctx context.Context) error {
-	if t.conn != nil {
-		if err := t.conn.Close(ctx); err != nil {
-			t.logger.LogAttrs(ctx, slog.LevelWarn, "Error while closing connection", slog.String("error", err.Error()))
-		}
-		t.conn = nil
-	}
-	conn, err := pgx.Connect(ctx, t.connString)
-	if err != nil {
-		return err
-	}
-	t.conn = conn
-	return nil
 }
