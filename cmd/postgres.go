0e016266324f49031b62bff49aa70ec373cb3111
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-09-11 13:53:53 +0300

    chore: update ruuvitag-common

--- cmd/postgres.go
+++ cmd/postgres.go
@@ -4,10 +4,7 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/niktheblak/ruuvitag-common/pkg/psql"
@@ -20,7 +17,7 @@ import (
 func createPostgresExporter(name string, columns map[string]string, cfg map[string]any) (exporter.Exporter, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	psqlInfo, err := createConnString(cfg)
+	psqlInfo, err := psql.CreateConnString(cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -32,65 +29,4 @@ func createPostgresExporter(name string, columns map[string]string, cfg map[stri
 		Columns:    columns,
 		Logger:     logger,
 	})
-}
-
-func createConnString(cfg map[string]any) (connString string, err error) {
-	var (
-		host     = cast.ToString(cfg["host"])
-		port     = cast.ToInt(cfg["port"])
-		username = cast.ToString(cfg["username"])
-		password = cast.ToString(cfg["password"])
-		database = cast.ToString(cfg["database"])
-		table    = cast.ToString(cfg["table"])
-		sslmode  = cast.ToString(cfg["sslmode"])
-		sslcert  = cast.ToString(cfg["sslcert"])
-		sslkey   = cast.ToString(cfg["sslkey"])
-	)
-	if host == "" {
-		err = fmt.Errorf("PostgreSQL host must be specified")
-		return
-	}
-	if database == "" {
-		err = fmt.Errorf("PostgreSQL database name must be specified")
-		return
-	}
-	if table == "" {
-		err = fmt.Errorf("PostgreSQL table name must be specified")
-		return
-	}
-	if sslmode == "" {
-		sslmode = "disable"
-	}
-	builder := new(strings.Builder)
-	builder.WriteString("host=")
-	builder.WriteString(host)
-	builder.WriteString(" ")
-	builder.WriteString("port=")
-	builder.WriteString(strconv.Itoa(port))
-	builder.WriteString(" ")
-	if username != "" {
-		builder.WriteString("user=")
-		builder.WriteString(username)
-		builder.WriteString(" ")
-	}
-	if password != "" {
-		builder.WriteString("password=")
-		builder.WriteString(password)
-		builder.WriteString(" ")
-	}
-	builder.WriteString("dbname=")
-	builder.WriteString(database)
-	builder.WriteString(" ")
-	builder.WriteString("sslmode=")
-	builder.WriteString(sslmode)
-	if sslcert != "" && sslkey != "" {
-		builder.WriteString(" ")
-		builder.WriteString("sslcert=")
-		builder.WriteString(sslcert)
-		builder.WriteString(" ")
-		builder.WriteString("sslkey=")
-		builder.WriteString(sslkey)
-	}
-	connString = builder.String()
-	return
 }
