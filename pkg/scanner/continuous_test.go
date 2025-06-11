bdef23e938846b251a685e77369ff976751155ab
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2025-06-11 10:27:21 +0300

    chore: fix another bunch of various linter warnings

--- pkg/scanner/continuous_test.go
+++ pkg/scanner/continuous_test.go
@@ -23,7 +23,10 @@ func TestScanContinuously(t *testing.T) {
 		Logger:        logger,
 	})
 	require.NoError(t, err)
-	defer scn.Close()
+	defer func() {
+		err := scn.Close()
+		require.NoError(t, err)
+	}()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	require.NoError(t, err)
 	errs := make(chan error, 1)
